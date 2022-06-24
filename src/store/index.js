964a1eb266e6d95f12847b45a1e4bd5f081e5e72
typhonrt <support@typhonjs.io>
2022-06-23 22:03:24 -0700

    Added storeCallback to `store` sub-package that wraps a writable store triggering a callback function directly when set is invoked.

--- src/store/index.js
+++ src/store/index.js
@@ -1,6 +1,8 @@
 export * from '@typhonjs-utils/dyn-array-reducer';
 
 export * from './functions.js';
+export * from './storeCallback.js';
+
 export * from './LocalStorage.js';
 export * from './SessionStorage.js';
 
