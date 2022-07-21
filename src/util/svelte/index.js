8c7b8b876b4fc8c17151c609f300751b79712e32
typhonrt <support@typhonjs.io>
2022-07-21 09:00:06 -0700

    Added isHMRProxy; fixed isApplicationShell / isSvelteComponent to detect proxy components.

--- src/util/svelte/index.js
+++ src/util/svelte/index.js
@@ -1,4 +1,5 @@
 export * from './isApplicationShell.js';
+export * from './isHMRProxy.js';
 export * from './isSvelteComponent.js';
 export * from './outroAndDestroy.js';
 export * from './parseSvelteConfig.js';
--- src/util/svelte/isSvelteComponent.js
+++ src/util/svelte/isSvelteComponent.js
@@ -9,6 +9,15 @@ export function isSvelteComponent(comp)
 {
    if (comp === null || comp === void 0 || typeof comp !== 'function') { return false; }
 
+   // When using Vite in a developer build the SvelteComponent is wrapped in a ProxyComponent class.
+   // This class doesn't define methods on the prototype, so we must check if the constructor name
+   // starts with `Proxy<` as it provides the wrapped component as `Proxy<_wrapped component name_>`.
+   const prototypeName = comp?.prototype?.constructor?.name;
+   if (typeof prototypeName === 'string' && (prototypeName.startsWith('Proxy<') || prototypeName === 'ProxyComponent'))
+   {
+      return true;
+   }
+
    return typeof window !== void 0 ?
     typeof comp.prototype.$destroy === 'function' && typeof comp.prototype.$on === 'function' : // client-side
      typeof comp.render === 'function'; // server-side
