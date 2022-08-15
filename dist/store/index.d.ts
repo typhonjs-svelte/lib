023aa8ee601d86759f3b30f3dec3680c333b7f29
typhonrt <support@typhonjs.io>
2022-08-15 02:06:08 -0700

    update dist

--- dist/store/index.d.ts
+++ dist/store/index.d.ts
@@ -1032,7 +1032,7 @@ declare class LocalStorage {
      *
      * @returns {LSStore} The new LSStore.
      */
-    static "__#215678@#createStore"(key: string, defaultValue?: boolean): svelte_store.Writable<any>;
+    static "__#215674@#createStore"(key: string, defaultValue?: boolean): svelte_store.Writable<any>;
     /**
      * Get value from the localStorage.
      *
@@ -1092,7 +1092,7 @@ declare class SessionStorage {
      *
      * @returns {SSStore} The new SSStore.
      */
-    static "__#215679@#createStore"(key: string, defaultValue?: boolean): svelte_store.Writable<any>;
+    static "__#215675@#createStore"(key: string, defaultValue?: boolean): svelte_store.Writable<any>;
     /**
      * Get value from the sessionStorage.
      *
