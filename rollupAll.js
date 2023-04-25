7d5c43af393b43a53b64d86e8bc4c25ed5757712
typhonrt <support@typhonjs.io>
2023-04-25 02:25:19 -0700

    Streamline build

--- rollupAll.js
+++ rollupAll.js
@@ -1,65 +1,20 @@
 import resolve             from '@rollup/plugin-node-resolve';
 import { generateDTS }     from '@typhonjs-build-test/esm-d-ts';
+import { importsExternal } from '@typhonjs-build-test/rollup-external-imports';
 import { rollup }          from 'rollup';
 
-const s_SOURCEMAPS = true;
+const sourcemap = true; // Defines whether source maps are generated.
 
-// Defines Svelte and all local exports as external.
-const s_LOCAL_EXTERNAL = [
-   'svelte', 'svelte/easing', 'svelte/internal', 'svelte/motion', 'svelte/store', 'svelte/transition',
-   'svelte/types',
-
-   '@typhonjs-svelte/lib/action', '@typhonjs-svelte/lib/animation', '@typhonjs-svelte/lib/handler',
-   '@typhonjs-svelte/lib/helper', '@typhonjs-svelte/lib/math', '@typhonjs-svelte/lib/store',
-   '@typhonjs-svelte/lib/transition', '@typhonjs-svelte/lib/util', '@typhonjs-svelte/lib/plugin/data',
-   '@typhonjs-svelte/lib/plugin/system'
-];
-
-// Defines potential output plugins to use conditionally if the .env file indicates the bundles should be
-// minified / mangled.
-const outputPlugins = [];
-
-// Defines whether source maps are generated / loaded from the .env file.
-const sourcemap = s_SOURCEMAPS;
-
-// GenerateDTS options -----------------------------------------------------------------------------------------------
-
-/**
- * Filter out "Duplicate identifier 'DOMRect'" messages.
- *
- * TODO: NOTE - The filtering of 2300 is unwanted churn, but 1014 can be a valid error though currently there is no
- * great way to describe destructuring rest parameters as a function argument with JSDoc that Typescript agrees with.
- * See this issue:
- *
- * @param {import('typescript').Diagnostic} diagnostic -
- *
- * @param {string} message -
- *
- * @returns {boolean} Return true to filter message.
- */
-const filterDiagnostic = (diagnostic, message) =>
- (diagnostic.code === 2300 && message === `Duplicate identifier 'DOMRect'.`) ||
- (diagnostic.code === 1014 && message === `A rest parameter must be last in a parameter list.`);
-
-// We don't care about external warning messages for `@typhonjs-svelte/lib` imports.
-const ignorePattern = /^@typhonjs-svelte\/lib/;
-
-const onwarn = (warning, warn) =>
-{
-   if (warning.code === 'UNRESOLVED_IMPORT' && ignorePattern.test(warning.exporter)) { return; }
-   warn(warning);
-};
-
-// Rollup plugin options for generateDTS.
-const dtsPluginOptions = { bundlePackageExports: true, filterDiagnostic, onwarn };
+// Bundle all top level external package exports.
+const dtsPluginOptions = { bundlePackageExports: true };
 
 // -------------------------------------------------------------------------------------------------------------------
 
 const rollupConfigs = [{
       input: {
          input: 'src/action/index.js',
-         external: s_LOCAL_EXTERNAL,
          plugins: [
+            importsExternal(),
             generateDTS.plugin(dtsPluginOptions)
          ]
       },
@@ -67,15 +22,14 @@ const rollupConfigs = [{
          file: 'dist/action/index.js',
          format: 'es',
          generatedCode: { constBindings: true },
-         plugins: outputPlugins,
          sourcemap
       }
    },
    {
       input: {
          input: 'src/animate/index.js',
-         external: s_LOCAL_EXTERNAL,
          plugins: [
+            importsExternal(),
             generateDTS.plugin(dtsPluginOptions)
          ]
       },
@@ -83,15 +37,14 @@ const rollupConfigs = [{
          file: 'dist/animate/index.js',
          format: 'es',
          generatedCode: { constBindings: true },
-         plugins: outputPlugins,
          sourcemap
       }
    },
    {
       input: {
          input: 'src/handler/index.js',
-         external: s_LOCAL_EXTERNAL,
          plugins: [
+            importsExternal(),
             generateDTS.plugin(dtsPluginOptions)
          ]
       },
@@ -99,15 +52,14 @@ const rollupConfigs = [{
          file: 'dist/handler/index.js',
          format: 'es',
          generatedCode: { constBindings: true },
-         plugins: outputPlugins,
          sourcemap
       }
    },
    {
       input: {
          input: 'src/helper/index.js',
-         external: s_LOCAL_EXTERNAL,
          plugins: [
+            importsExternal(),
             generateDTS.plugin(dtsPluginOptions)
          ]
       },
@@ -115,15 +67,14 @@ const rollupConfigs = [{
          file: 'dist/helper/index.js',
          format: 'es',
          generatedCode: { constBindings: true },
-         plugins: outputPlugins,
          sourcemap
       }
    },
    {
       input: {
          input: 'src/math/index.js',
-         external: s_LOCAL_EXTERNAL,
          plugins: [
+            importsExternal(),
             resolve(),
             generateDTS.plugin({ ...dtsPluginOptions, replace: {
                '/// <reference types="gl-matrix/index.js" />': '',  // Remove reference in dist/math/index.d.ts
@@ -135,15 +86,14 @@ const rollupConfigs = [{
          file: 'dist/math/index.js',
          format: 'es',
          generatedCode: { constBindings: true },
-         plugins: outputPlugins,
          sourcemap
       }
    },
    {
       input: {
          input: 'src/store/index.js',
-         external: s_LOCAL_EXTERNAL,
          plugins: [
+            importsExternal(),
             resolve(),
             generateDTS.plugin(dtsPluginOptions)
          ]
@@ -152,38 +102,30 @@ const rollupConfigs = [{
          file: 'dist/store/index.js',
          format: 'es',
          generatedCode: { constBindings: true },
-         plugins: outputPlugins,
          sourcemap
       }
    },
    {
       input: {
          input: 'src/store/position/index.js',
-         external: s_LOCAL_EXTERNAL,
          plugins: [
+            importsExternal(),
             resolve(),
-            generateDTS.plugin({
-               ...dtsPluginOptions,
-               prependGen: ['src/store/position/typedefs.js'],
-               prependString: [
-                  'export { TJSPositionInitialHelper, TJSPositionValidatorOptions, TJSPositionDataExtended, TJSTransformOrigin };'
-               ]
-            })
+            generateDTS.plugin(dtsPluginOptions)
          ]
       },
       output: {
          file: 'dist/store/position/index.js',
          format: 'es',
          generatedCode: { constBindings: true },
-         plugins: outputPlugins,
          sourcemap
       }
    },
    {
       input: {
          input: 'src/transition/index.js',
-         external: s_LOCAL_EXTERNAL,
          plugins: [
+            importsExternal(),
             resolve(),
             generateDTS.plugin(dtsPluginOptions)
          ]
@@ -192,15 +134,14 @@ const rollupConfigs = [{
          file: 'dist/transition/index.js',
          format: 'es',
          generatedCode: { constBindings: true },
-         plugins: outputPlugins,
          sourcemap
       }
    },
    {
       input: {
          input: 'src/util/index.js',
-         external: s_LOCAL_EXTERNAL,
          plugins: [
+            importsExternal(),
             resolve(),
             generateDTS.plugin(dtsPluginOptions)
          ]
@@ -209,15 +150,14 @@ const rollupConfigs = [{
          file: 'dist/util/index.js',
          format: 'es',
          generatedCode: { constBindings: true },
-         plugins: outputPlugins,
          sourcemap
       }
    },
    {
       input: {
          input: 'src/plugin/data/index.js',
-         external: s_LOCAL_EXTERNAL,
          plugins: [
+            importsExternal(),
             generateDTS.plugin(dtsPluginOptions)
          ]
       },
@@ -225,15 +165,14 @@ const rollupConfigs = [{
          file: 'dist/plugin/data/index.js',
          format: 'es',
          generatedCode: { constBindings: true },
-         plugins: outputPlugins,
          sourcemap
       }
    },
    {
       input: {
          input: 'src/plugin/system/index.js',
-         external: s_LOCAL_EXTERNAL,
          plugins: [
+            importsExternal(),
             resolve(),
             generateDTS.plugin(dtsPluginOptions)
          ]
@@ -242,7 +181,6 @@ const rollupConfigs = [{
          file: 'dist/plugin/system/index.js',
          format: 'es',
          generatedCode: { constBindings: true },
-         plugins: outputPlugins,
          sourcemap
       }
    }
