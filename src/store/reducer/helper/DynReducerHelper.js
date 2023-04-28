23b83a909678d37cf3877322bfb14e0937d13d3f
typhonrt <support@typhonjs.io>
2023-04-28 14:22:38 -0700

    eslint fix

--- src/store/reducer/helper/DynReducerHelper.js
+++ src/store/reducer/helper/DynReducerHelper.js
@@ -13,7 +13,7 @@ export class DynReducerHelper
     *
     * @returns {{
     *    regexObjectQuery: (properties: string|Iterable<string>, options?: {caseSensitive?: boolean, store?: import('#svelte/store').Writable<string>}) => (((data: {}) => boolean) & import('#svelte/store').Writable<string>)
-    * }}
+    * }} All available filters.
     */
    static get filters() { return filters; }
 }
