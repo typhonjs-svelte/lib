6d07496f1b160d35c7e4bad4a522b8fb2836b346
typhonrt <support@typhonjs.io>
2023-06-27 20:04:16 -0700

    removed store/reducer sub-path export

--- rollupAll.js
+++ rollupAll.js
@@ -106,22 +106,6 @@ const rollupConfigs = [
    },
    {
       input: {
-         input: 'src/store/reducer/index.js',
-         plugins: [
-            importsExternal(),
-            resolve(),
-            generateDTS.plugin(dtsPluginOptions)
-         ]
-      },
-      output: {
-         file: '_dist/store/reducer/index.js',
-         format: 'es',
-         generatedCode: { constBindings: true },
-         sourcemap
-      }
-   },
-   {
-      input: {
          input: 'src/store/storage/web/index.js',
          plugins: [
             importsExternal(),
--- src/store/reducer/index.js
+++
@@ -1,2 +0,0 @@
-export * from './array-object/index.js';
-export * from './helper/index.js';
